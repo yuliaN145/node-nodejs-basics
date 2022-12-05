f22564a82a67ee3ef11652566e009e35b3404534
Yulia <Yulia159V@gmail.com>
2022-12-05 22:37:14 +0300

    feat: add solution

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,11 @@
+import { createReadStream } from "fs";
+import * as url from 'url';
+    const __filename = url.fileURLToPath(import.meta.url);
+    const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 const read = async () => {
-    // Write your code here 
+  const readStream = createReadStream('src/streams/files/fileToRead.txt');
+  readStream.pipe(process.stdout);
 };
 
 await read();
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,23 @@
+import { Transform } from "stream";
+
 const transform = async () => {
-    // Write your code here 
+    // const reverseStream = new Transform({
+    //     transform(chunk, encoding, callback) {
+    //       this.push(chunk.toString().split("").reverse().join(""));
+    //       callback();
+    //     },
+    //   }); 
+      const reverseStream = new Transform({
+        transform(chunk, encoding, callback) {
+            callback(null, chunk.toString().split('').reverse().join(''));
+        }
+    });
+      process.stdin.on("data", (data) => {
+        reverseStream.write(data);
+        reverseStream.end();
+        process.exit();
+      });
+      reverseStream.pipe(process.stdout);
 };
 
 await transform();
